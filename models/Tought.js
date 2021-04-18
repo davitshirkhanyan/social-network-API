3479eac39fefe2b9e2f7c13ca8fb9ee15bdc75c0
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-04-17 23:40:58 -0700

    created reaction subdocument

--- models/Tought.js
+++ models/Tought.js
@@ -1,6 +1,35 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema (
+    {
+        // set custom id to avoid confusion with parent tought _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 const ToughtSchema = new Schema (
     {
         toughtText: {
@@ -19,6 +48,7 @@ const ToughtSchema = new Schema (
             required: true,
             ref: 'User'
         },
+        // use ReactionSchema to validate data for a reaction
         reactions: [ReactionSchema],
     },
     {
