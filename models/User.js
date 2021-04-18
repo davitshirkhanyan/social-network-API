fd7dc09bb9125c9870132825bd1f4e2184fca713
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-04-17 22:41:12 -0700

    created User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Tought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+// create the User Model using the Schema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
