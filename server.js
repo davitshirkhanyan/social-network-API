a99e4108f675b3e3ffb2738a2746e8375e60e87f
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-04-18 18:16:39 -0700

    change the code

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true
+// });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
